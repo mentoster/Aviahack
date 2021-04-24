62ee6aa0bab05a542d118002ceb46f61e521b58c
Dmitriy Makkarov <Dimamakarov1941@yandex.ru>
2021-04-24 17:36:28 +0300

    add floating button

--- lib/map/gmap.dart
+++ lib/map/gmap.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -24,6 +25,8 @@ class GmapState extends State<Gmap> {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    print(size.width);
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
@@ -32,12 +35,73 @@ class GmapState extends State<Gmap> {
           _controller.complete(controller);
         },
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheAirport,
-        label: Text('Показать аэропорт'),
-        icon: Icon(Icons.room_outlined),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: SpeedDial(
+        /// both default to 16
+        marginEnd: size.width - 90,
+        marginBottom: 40,
+        // animatedIcon: AnimatedIcons.menu_close,
+        // animatedIconTheme: IconThemeData(size: 22.0),
+        /// This is ignored if animatedIcon is non null
+        icon: Icons.add,
+        activeIcon: Icons.remove,
+        // iconTheme: IconThemeData(color: Colors.grey[50], size: 30),
+        /// The label of the main button.
+        // label: Text("Open Speed Dial"),
+        /// The active label of the main button, Defaults to label if not specified.
+        // activeLabel: Text("Close Speed Dial"),
+        /// Transition Builder between label and activeLabel, defaults to FadeTransition.
+        // labelTransitionBuilder: (widget, animation) => ScaleTransition(scale: animation,child: widget),
+        /// The below button size defaults to 56 itself, its the FAB size + It also affects relative padding and other elements
+        buttonSize: 80.0,
+        visible: true,
+
+        /// If true user is forced to close dial manually
+        /// by tapping main button and overlay is not rendered.
+        closeManually: false,
+
+        /// If true overlay will render no matter what.
+        renderOverlay: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
         backgroundColor: Colors.orange[700],
+        foregroundColor: Colors.white,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        // orientation: SpeedDialOrientation.Up,
+        // childMarginBottom: 2,
+        // childMarginTop: 2,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.looks_one),
+            backgroundColor: Colors.white,
+            label: 'First',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('FIRST CHILD'),
+            onLongPress: () => print('FIRST CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.looks_two),
+            backgroundColor: Colors.white,
+            label: 'Second',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('SECOND CHILD'),
+            onLongPress: () => print('SECOND CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.looks_3),
+            backgroundColor: Colors.white,
+            label: 'Third',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('THIRD CHILD'),
+            onLongPress: () => print('THIRD CHILD LONG PRESS'),
+          ),
+        ],
       ),
     );
   }
