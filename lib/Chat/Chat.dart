a41ece56fa1a9fb68dc053cc2b8509f40017c5fd
Dmitriy Makkarov <Dimamakarov1941@yandex.ru>
2021-04-25 02:35:59 +0300

    stupid fix

--- lib/Chat/Chat.dart
+++ lib/Chat/Chat.dart
@@ -12,7 +12,7 @@ class Chat extends StatefulWidget {
 }
 
 class _Chat extends State<Chat> {
-  int numberOfScanMessage = 0;
+  int numberOfScanMessage = 1;
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
   AirRoad askInfo = new AirRoad();
@@ -27,7 +27,6 @@ class _Chat extends State<Chat> {
 
   void waitAnswer(query) async {
     Response(query);
-    numberOfScanMessage = 1;
   }
 
   Widget _buildTextComposer() {
